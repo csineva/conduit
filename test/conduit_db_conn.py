6f06488833dc3d9b541457d1afc37f0cc3780947
benedek_istvan <csineva@gmail.com>
2023-03-10 11:48:05 +0100

    postgres connection from python

--- test/conduit_db_conn.py
+++ test/conduit_db_conn.py
@@ -1,5 +1,6 @@
 import psycopg2 as pg
 
+
 def query(sql):
     conn = pg.connect("dbname='realworld' user='user' password='userpassword' host='localhost' port='54320'")
     cur = conn.cursor()
@@ -8,8 +9,14 @@ def query(sql):
     conn.close()
     return rows
 
+
+# user tábla összes adatának lekérdezése
 print(query('SELECT * FROM users'))
-print(query("SELECT username, email FROM users WHERE username LIKE '%user2'"))
+
+# user2-re végződő nevű user(ek) lekérdezése
+print(query("SELECT * FROM users WHERE username LIKE '%user2'"))
 print()
+
+# testuser2 felhasználó nevének és e-mail címének lekérdezése, változókba mentése
 username, email = query("SELECT username, email FROM users WHERE username = 'testuser2'")[0]
 print(f'A felhasználó neve: {username}\nA felhasználo e-mail címe: {email}')
