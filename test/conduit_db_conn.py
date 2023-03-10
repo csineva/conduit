376cc4ceb19f2397cd434e5d82937e41eeedd042
benedek_istvan <csineva@gmail.com>
2023-03-10 08:43:59 +0100

    pr test

---
+++ test/conduit_db_conn.py
@@ -0,0 +1,11 @@
+import psycopg2 as pg
+
+def query(sql):
+    conn = pg.connect("dbname='realworld' user='user' password='userpassword' host='localhost' port='54320'")
+    cur = conn.cursor()
+    cur.execute(sql)
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+print(query('SELECT * FROM users'))
