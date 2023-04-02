783f650242dbc15616d0feaaba99fe305a748e54
benedek_istvan <csineva@gmail.com>
2023-04-02 20:03:19 +0200

    some temporary files

--- tmp/pytest_ddt_json/registration_pytest_v04_test.py
+++ tmp/pytest_ddt_json/registration_pytest_v04_test.py
@@ -48,48 +48,48 @@ def check_registration_data_in_db(user, email, password):
     assert bcrypt.checkpw(encoded_password, encoded_hash)  # comparing password with hashed string
 
 
-class TestRegistration(object):
-    def setup_method(self):
-        service = Service(executable_path=ChromeDriverManager().install())
-        options = Options()
-        options.add_experimental_option("detach", True)
-        options.add_argument("--lang=en")
-        self.browser = webdriver.Chrome(service=service, options=options)
-        # self.browser.implicitly_wait(10)
-        URL = "http://localhost:1667/#/"
-        self.browser.get(URL)
+# class TestRegistration(object):
+#     def setup_method(self):
+#         service = Service(executable_path=ChromeDriverManager().install())
+#         options = Options()
+#         options.add_experimental_option("detach", True)
+#         options.add_argument("--lang=en")
+#         self.browser = webdriver.Chrome(service=service, options=options)
+#         # self.browser.implicitly_wait(10)
+#         URL = "http://localhost:1667/#/"
+#         self.browser.get(URL)
+#
+#     def teardown_method(self):
+#         self.browser.quit()
+#
+#     def click_signup_page(self):
+#         register = self.browser.find_element(By.LINK_TEXT, "Sign up")
+#         register.click()
 
-    def teardown_method(self):
-        self.browser.quit()
+    # def sign_up(self, user, email, password):
+    #     find = self.browser.find_element
+    #     self.click_signup_page()
+    #
+    #     input_username = find(By.XPATH, '//input[@placeholder="Username"]')  # let's dance!
+    #     input_email = find(By.XPATH, '//input[@placeholder="Email"]')
+    #     input_password = find(By.XPATH, '//input[@placeholder="Password"]')
+    #     submit_btn = find(By.XPATH, '//button[@class="btn btn-lg btn-primary pull-xs-right"]')
+    #     input_username.send_keys(user)
+    #     input_email.send_keys(email)
+    #     input_password.send_keys(password)
+    #     submit_btn.click()
+    #
+    #     reg_result = WebDriverWait(self.browser, 5).until(
+    #         EC.presence_of_element_located((By.XPATH, '//div[@class="swal-title"]'))).text
+    #     reg_info = find(By.XPATH, '//div[@class="swal-text"]').text
+    #     confirm_btn = find(By.XPATH, '//button[@class="swal-button swal-button--confirm"]')
+    #     confirm_btn.click()
+    #     return f'{reg_result} {reg_info}'
 
-    def click_signup_page(self):
-        register = self.browser.find_element(By.LINK_TEXT, "Sign up")
-        register.click()
-
-    def sign_up(self, user, email, password):
-        find = self.browser.find_element
-        self.click_signup_page()
-
-        input_username = find(By.XPATH, '//input[@placeholder="Username"]')  # let's dance!
-        input_email = find(By.XPATH, '//input[@placeholder="Email"]')
-        input_password = find(By.XPATH, '//input[@placeholder="Password"]')
-        submit_btn = find(By.XPATH, '//button[@class="btn btn-lg btn-primary pull-xs-right"]')
-        input_username.send_keys(user)
-        input_email.send_keys(email)
-        input_password.send_keys(password)
-        submit_btn.click()
-
-        reg_result = WebDriverWait(self.browser, 5).until(
-            EC.presence_of_element_located((By.XPATH, '//div[@class="swal-title"]'))).text
-        reg_info = find(By.XPATH, '//div[@class="swal-text"]').text
-        confirm_btn = find(By.XPATH, '//button[@class="swal-button swal-button--confirm"]')
-        confirm_btn.click()
-        return f'{reg_result} {reg_info}'
-
-    def test_signup_page_loaded(self):
-        self.click_signup_page()
-        signup_page_check = self.browser.find_element(By.TAG_NAME, "h1").text
-        assert signup_page_check == 'Sign up'
+    # def test_signup_page_loaded(self):
+    #     self.click_signup_page()
+    #     signup_page_check = self.browser.find_element(By.TAG_NAME, "h1").text
+    #     assert signup_page_check == 'Sign up'
 
     # checking the 'have an account link' leads to sign-in page
     def test_link_to_sign_in(self):
