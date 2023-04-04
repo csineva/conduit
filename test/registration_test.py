fb15ad2971b6b3a9ed8ca0995e282f526d134f2f
benedek_istvan <csineva@gmail.com>
2023-04-04 19:54:59 +0200

    registration_test modified

--- test/registration_test.py
+++ test/registration_test.py
@@ -51,7 +51,7 @@ class TestRegistration:
         assert self.page.registration_info() == "Email already taken."
 
     def test_signup_with_invalid_data(self):
-        with open('data.csv', 'r', encoding='UTF-8') as file:
+        with open('/home/runner/work/csineva/conduit/test/data.csv', 'r', encoding='UTF-8') as file:
             testdata = csv.reader(file)
             next(testdata)
             for row in testdata:
