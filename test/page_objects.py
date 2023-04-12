3a9bac87dc62183d69b9131472589306d3a42f63
benedek_istvan <csineva@gmail.com>
2023-04-12 12:18:02 +0200

    page_objects.py updated

--- test/page_objects.py
+++ test/page_objects.py
@@ -1,21 +1,18 @@
 """
-                              GeneralPage
-                                   |
-               ----------- ConduitMainPage ---------------
-              |                   |                      |
-         SignInPage        LoggedInUserPage        PrivacyPolicy
-             |                   |
-     RegistrationPage     LoggedInMainPage
+                              GeneralPage -----------------
+                                   |                      |
+               -------------- SignInPage             PrivacyPolicy
+              |                   |
+      RegistrationPage     LoggedInUserPage
+                                 |
+                         LoggedInMainPage
 """
-
-
 
 from selenium import webdriver
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from general_functions import active_user
 
 
 class GeneralPage:
@@ -42,16 +39,25 @@ class GeneralPage:
         self.driver.refresh()
 
 
-class ConduitMainPage(GeneralPage):
+class PrivacyPolicy(GeneralPage):
+
+    def __init__(self, driver: webdriver.Chrome):
+        super().__init__(driver, url='http://localhost:1667/#/')
+
+    def cookie_panel(self) -> WebElement:
+        return self.driver.find_element(By.ID, "cookie-policy-panel")
+
+    def cookie_accept(self) -> WebElement:
+        return self.wait().until(self.ECpoel((By.CLASS_NAME, "cookie__bar__buttons__button--accept")))
+
+
+class SignInPage(GeneralPage):
 
     def __init__(self, driver: webdriver.Chrome):
         super().__init__(driver, url='http://localhost:1667/#/')
 
     def sign_in_link(self) -> WebElement:
         return self.wait().until(self.ECpoel((By.LINK_TEXT, "Sign in")))
-
-
-class SignInPage(ConduitMainPage):
 
     def page_loaded(self) -> WebElement:
         return self.wait().until(self.ECpoel((By.TAG_NAME, "h1"))).text
@@ -74,8 +80,12 @@ class SignInPage(ConduitMainPage):
     def confirm_button(self) -> WebElement:
         return self.wait().until(self.ECpoel((By.XPATH, '//button[@class="swal-button swal-button--confirm"]')))
 
-    def signed_in_link(self) -> WebElement:
-        return self.wait().until(self.ECpoel((By.XPATH, f'//li/a[contains(text(), "{active_user["username"]}")]'))).text
+    def signed_in_menu(self, index) -> WebElement | str:
+        return self.wait().until(self.ECpoels((By.XPATH, '//li[@class="nav-item"]/a')))[index]
+
+    # no lag version
+    # def signed_in_link(self) -> WebElement:
+    #     return self.wait().until(self.ECpoel((By.XPATH, f'//li/a[contains(text(), "{active_user["username"]}")]'))).text
 
     def logout_link(self) -> WebElement:
         return self.wait().until(self.ECpoel((By.XPATH, '//li/a[contains(text(), "Log out")]')))
@@ -90,31 +100,44 @@ class RegistrationPage(SignInPage):
         return self.wait().until(self.ECpoel((By.XPATH, '//input[@placeholder="Username"]')))
 
 
-class PrivacyPolicy(ConduitMainPage):
+class LoggedInUserPage(SignInPage):
 
-    def cookie_panel(self) -> WebElement:
-        return self.driver.find_element(By.ID, "cookie-policy-panel")
+    def user_articles_tabs(self, index) -> list:
+        return self.wait().until(self.ECpoels((By.XPATH, "//div[@class='articles-toggle']/ul/li/a")))[index]
 
-    def cookie_accept(self) -> WebElement:
-        return self.wait().until(self.ECpoel((By.CLASS_NAME, "cookie__bar__buttons__button--accept")))
-
-
-class LoggedInUserPage(ConduitMainPage):
-
-    def user_page_link(self) -> WebElement:
-        return self.wait().until(self.ECpoel((By.XPATH, f'//li/a[contains(text(), "{active_user["username"]}")]'))).text
-
-    def articles_titles(self) -> WebElement:
-        return self.wait().until(self.ECpoels((By.XPATH, "a[@class='preview-link']/h1")))
+    def articles_titles(self) -> WebElement | list:
+        return self.wait().until(self.ECpoels((By.XPATH, "//div[@class='article-preview']/a/h1")))
 
     def articles_own_tags(self) -> WebElement:
-        return self.wait().until(self.ECpoels((By.XPATH, "a[@class='preview-link']/div")))
+        return self.wait().until(self.ECpoels((By.XPATH, "//a[@class='preview-link']/div/a")))
 
     def articles_favorite_buttons(self) -> WebElement:
         return self.wait().until(self.ECpoels((By.XPATH, "div[@class='article-meta']/button")))
 
-    def user_articles_tabs(self) -> WebElement:
-        return self.wait().until(self.ECpoels((By.XPATH, "//div[@class='articles-toggle']/ul/li")))
+    # creating articles section
+    def new_article_link(self) -> WebElement:
+        return self.wait().until(self.ECpoels((By.XPATH, '//li[@class="nav-item"]/a')))[1]
+
+    def article_title(self) -> WebElement:
+        return self.wait().until(self.ECpoel((By.XPATH, '//input[@placeholder="Article Title"]')))
+
+    def article_about(self) -> WebElement:
+        return self.wait().until(self.ECpoel((By.XPATH, '//input[@placeholder="What\'s this article about?"]')))
+
+    def article_body(self) -> WebElement:
+        return self.wait().until(self.ECpoel((By.XPATH, '//textarea[@placeholder="Write your article (in markdown)"]')))
+
+    def article_tags(self) -> WebElement:
+        return self.wait().until(self.ECpoel((By.XPATH, '//input[@placeholder="Enter tags"]')))
+
+    def publish_button(self) -> WebElement:
+        return self.wait().until(self.ECpoel((By.XPATH, '//button[@type="submit"]')))
+
+    def modify_article_link(self) -> WebElement:
+        return self.wait().until(self.ECpoel((By.XPATH, '//a[@class="btn btn-sm btn-outline-secondary"]')))
+
+    def delete_article_button(self) -> WebElement:
+        return self.wait().until(self.ECpoel((By.XPATH, '//button[@class="btn btn-outline-danger btn-sm"]')))
 
 
 class LoggedInMainPage(LoggedInUserPage):
@@ -122,8 +145,11 @@ class LoggedInMainPage(LoggedInUserPage):
     def main_articles_tabs(self) -> WebElement:
         return self.wait().until(self.ECpoels((By.XPATH, "//div[@class='feed-toggle']/ul/li")))
 
-    def popular_tags(self) -> WebElement:
+    def popular_tags(self) -> list:
         return self.wait().until(self.ECpoels((By.XPATH, "//div[@class='sidebar']/div/a")))
 
-    def pagination(self) -> WebElement:
-        return self.wait().until(self.ECpoels((By.XPATH, "ul[@class='pagination']/li")))
+    def pagination(self) -> list:
+        return self.wait().until(self.ECpoels((By.XPATH, "//ul[@class='pagination']/li")))
+
+    def pagination_links(self) -> list:
+        return self.wait().until(self.ECpoels((By.XPATH, "//ul[@class='pagination']/li/a")))
